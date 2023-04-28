3eac78502e62187c07a63cbac23004b65bb2a4da
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2023-04-28 13:03:20 +0300

    Add project files.

---
+++ EsiniBul/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace EsiniBul
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlKartlar = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlKartlar
+            // 
+            this.pnlKartlar.BackColor = System.Drawing.Color.Gainsboro;
+            this.pnlKartlar.Location = new System.Drawing.Point(12, 12);
+            this.pnlKartlar.Name = "pnlKartlar";
+            this.pnlKartlar.Size = new System.Drawing.Size(505, 441);
+            this.pnlKartlar.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(527, 464);
+            this.Controls.Add(this.pnlKartlar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Eşini Bul";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel pnlKartlar;
+    }
+}
---
+++ EsiniBul/Form1.cs
@@ -0,0 +1,154 @@
+﻿using EsiniBul.Properties;
+
+namespace EsiniBul
+{
+    public partial class Form1 : Form
+    {
+        int boyut = 4;
+        int w = 128;
+        List<int> resimler;
+        List<PictureBox> aciklar;
+        int gizlenenAdet;
+
+        public Form1()
+        {
+            InitializeComponent();
+            YeniOyunHazirla();
+        }
+
+        private void ResimleriBelirle()
+        {
+            int toplamKartAdet = boyut * boyut;
+            int resimCesitAdet = toplamKartAdet / 2;
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 1; j <= resimCesitAdet; j++)
+                {
+                    resimler.Add(j);
+                }
+            }
+            ListeKaristir(resimler);
+        }
+
+        private void ListeKaristir(List<int> resimler)
+        {
+            Random rnd = new Random();
+            int temp, talihliIndeks;
+
+            for (int i = 0; i < resimler.Count - 1; i++)
+            {
+                talihliIndeks = rnd.Next(i, resimler.Count);
+                temp = resimler[i];
+                resimler[i] = resimler[talihliIndeks];
+                resimler[talihliIndeks] = temp;
+            }
+        }
+
+        private void KartlariOlustur()
+        {
+            int no = 0;
+            for (int i = 0; i < boyut; i++)
+            {
+                for (int j = 0; j < boyut; j++)
+                {
+                    PictureBox pb = new PictureBox();
+                    pb.Size = new Size(w, w);
+                    pb.Location = new Point(j * w, i * w);
+                    pb.SizeMode = PictureBoxSizeMode.Zoom;
+                    pb.Image = Resim(0);
+                    pb.Tag = no;
+                    pb.Click += Pb_Click;
+                    pnlKartlar.Controls.Add(pb);
+                    no++;
+                }
+            }
+            pnlKartlar.Size = new Size(boyut * w, boyut * w);
+            ClientSize = new Size(boyut * w + 24, boyut * w + 24);
+        }
+
+        // hangi picturebox'a tıklandıysa sender'da o vardır
+        private void Pb_Click(object sender, EventArgs e)
+        {
+            PictureBox tiklanan = (PictureBox)sender;
+            int tiklananNo = (int)tiklanan.Tag;
+
+            // Eğer tıklanan kart zaten açıksa hiçbir şey yapmadan çık
+            if (aciklar.Contains(tiklanan))
+                return;
+
+            // Eğer zaten açık 2 kart varsa önce onları kapat
+            if (aciklar.Count == 2)
+            {
+                AciklariKapat();
+            }
+
+            aciklar.Add(tiklanan);
+
+            // Eğer açık kart sayısı 2 ise ve bunlar aynıysa onları gizle
+            int oncekiNo = (int)aciklar[0].Tag;
+            tiklanan.Image = Resim(resimler[tiklananNo]);
+
+            if (aciklar.Count == 2 && resimler[oncekiNo] == resimler[tiklananNo])
+            {
+                // gizlenmeden önce yarım saniye görme şansı ver
+                Update(); // tıklananı hemen göster
+                Thread.Sleep(500);
+                AciklariGizle();
+            }
+
+            // Eğer tüm kartlar gizlendiyse o zaman oyunu bitir
+            if (gizlenenAdet == resimler.Count)
+            {
+                DialogResult cevap = MessageBox.Show(
+                    "Oyun Bitti! Tekrar oynamak ister misiniz?", 
+                    "Tekrar Oyna?", 
+                    MessageBoxButtons.YesNo, 
+                    MessageBoxIcon.Question, 
+                    MessageBoxDefaultButton.Button1);
+
+                if (cevap == DialogResult.Yes)
+                {
+                    YeniOyunHazirla();
+                }
+                else
+                {
+                    Close(); // pencereyi kapat
+                }
+            }
+        }
+
+        private void YeniOyunHazirla()
+        {
+            pnlKartlar.Controls.Clear();
+            resimler = new List<int>();
+            aciklar = new List<PictureBox>();
+            gizlenenAdet = 0;
+            ResimleriBelirle();
+            KartlariOlustur();
+        }
+
+        private void AciklariGizle()
+        {
+            gizlenenAdet += 2;
+            foreach (PictureBox pb in aciklar)
+            {
+                pb.Hide();
+            }
+            aciklar.Clear();
+        }
+
+        private void AciklariKapat()
+        {
+            foreach (PictureBox pb in aciklar)
+            {
+                pb.Image = Resim(0);
+            }
+            aciklar.Clear();
+        }
+
+        Bitmap Resim(int no)
+        {
+            return (Bitmap)Resources.ResourceManager.GetObject(no.ToString());
+        }
+    }
+}
